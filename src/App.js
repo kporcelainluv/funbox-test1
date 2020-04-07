944bb99899023a5203ce1ffec20123f6d75abbf9
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-04-07 18:34:32 +0300

    add return statement, refactor css background

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ const handleOutput = products => {
       chosen = [...chosen, name];
       return chosen;
     }
+    return product;
   });
 
   return `Пользователь выбрал: ${handleText(chosen)}
